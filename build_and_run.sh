fd8f5647a18e495137efa7cf7d531700cdee8afd
KAdenrele <akomolafe@zaizi.com>
2026-03-30 12:56:51 +0100

    updated mapped volume to use data instead of data3

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,5 +2,5 @@ docker build -t wan2_2_auto . && docker run --rm \
                                             --gpus "device=0" \
                                             --ipc=host \
                                             -v /mnt/data/test_dataset/models:/workspace/Wan2.2/Wan2.2-TI2V-5B \
-                                            -v /mnt/data3/video_clustering/videos/Wanimate:/workspace/Wan2.2/outputs \
+                                            -v /mnt/data/video_clustering/videos/Wanimate:/workspace/Wan2.2/outputs \
                                             wan2_2_auto
